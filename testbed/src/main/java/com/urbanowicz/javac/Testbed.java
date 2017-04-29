f525b65adfbb8fdfa9f049afe86eb0e62b31cac9
Dmitri Urbanowicz <hun10@yandex.ru>
2017-04-29 13:19:33 +0400

    Resorted to https://github.com/amelentev/java-oo

--- testbed/src/main/java/com/urbanowicz/javac/Testbed.java
+++ testbed/src/main/java/com/urbanowicz/javac/Testbed.java
@@ -6,7 +6,7 @@ import java.util.Map;
 public class Testbed {
     public static void main(String... args) {
         Map<String, String> map = new HashMap<>();
-        map.put("2+2", "4");
+        map["2+2"] = "5";
         System.out.println(map["2+2"]);
     }
 }
